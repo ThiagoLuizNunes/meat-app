ecee88c3a039c789cce7d30a43f944709d62d8e8
ThiagoLuizNunes <thiagoluiz.dev@gmail.com>
2018-12-26 17:23:40 -0300

    create not-foun and order-summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
